d7cbf76ef69c2822a7f35b17ed0243dd6f9fb906
Pranaov <pranaov5925@gmail.com>
2025-11-08 21:37:25 +0530

    added game-status ui

--- app.jsx
+++ app.jsx
@@ -8,6 +8,10 @@ export default function AssemblyEndgame() {
                 <p>Guess the word within 8 attempts to keep the 
                 programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
