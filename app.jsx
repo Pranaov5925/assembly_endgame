38d38b96bad77445a1e925df94a4672718a3f98f
Pranaov <pranaov5925@gmail.com>
2025-11-10 19:06:45 +0530

    added gameOver logic

--- app.jsx
+++ app.jsx
@@ -8,6 +8,10 @@ export default function AssemblyEndgame() {
 
     const wrongGuessCount = 
         guessedLetters.filter(letter => !currentWord.includes(letter)).length
+    const isGameWon = 
+        currentWord.split("").every(letter => guessedLetters.includes(letter))
+    const isGameLost = wrongGuessCount >= languages.length - 1
+    const isGameOver = isGameWon || isGameLost
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
@@ -63,6 +67,33 @@ export default function AssemblyEndgame() {
             </button>
         )})
 
+    const gameStatusClass = clsx("game-status", {
+        won: isGameWon,
+        lost: isGameLost
+    })
+
+    function renderGameStatus() {
+        if (!isGameOver) {
+            return null
+        }
+
+        if (isGameWon) {
+            return (
+                <>
+                    <h2>You win!</h2>
+                    <p>Well done! 🎉</p>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <h2>Game over!</h2>
+                    <p>You lose! Better start learning Assembly 😭</p>
+                </>
+            )
+        }
+    }
+
     return (
         <main>
             <header>
@@ -70,9 +101,8 @@ export default function AssemblyEndgame() {
                 <p>Guess the word within 8 attempts to keep the 
                 programming world safe from Assembly!</p>
             </header>
-            <section className="game-status">
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
+            <section className={gameStatusClass}>
+                {renderGameStatus()}
             </section>
             <section className="language-chips">
                 {languageElements}
@@ -83,7 +113,7 @@ export default function AssemblyEndgame() {
             <section className="keyboard">
                 {keyboardElements}
             </section>
-            <button className="new-game">New Game</button>
+            {isGameOver && <button className="new-game">New Game</button>}
         </main>
     )
 }
